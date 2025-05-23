4d31b1dd5a160433c6852388679f15e2622c3bb6
z389026507 <zhuangjiangshan@gmail.com>
2025-05-23 15:01:13 +0800

    Add files via upload
    fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-export default {
-  root: "./src",
-  server: { port: 3000 },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: "./src"
+})
